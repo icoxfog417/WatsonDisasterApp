361c96937c21ea241ab0a9a58e0b7a97b9596e47
SnowMasaya <snow.akogi.pgel@gmail.com>
2015-12-05 20:47:12 +0900

    fix category.py

--- app/model/category.py
+++ app/model/category.py
@@ -2,4 +2,15 @@ from enum import Enum
 
 
 class Category(Enum):
-    NoSetting = "NoSetting"
+    LifeConFirmation     = "安否確認"
+    HelpObject           = "物資要請"
+    AssistantRequest     = "救助"
+    HelpRequest          = "支援要請"
+    LifeConFirmation     = "安否確認"
+    Emergency            = "緊急支援"
+    LifeLine             = "ライフライン"
+    TransPortation       = "交通機関"
+    HouseInformation     = "住宅情報"
+    HealthInformation    = "医療・福祉・健康相談"
+    LifeCareInformation  = "生活支援・相談"
+    NoSetting            = "NoSetting"
