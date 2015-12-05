148f94d6b24063b06bd10f0b441171747fbbb23d
SnowMasaya <snow.akogi.pgel@gmail.com>
2015-12-06 08:18:16 +0900

    update test_watson.py
    fix japanese text test

--- app/model/category.py
+++ app/model/category.py
@@ -6,7 +6,6 @@ class Category(Enum):
     HelpObject           = "物資要請"
     AssistantRequest     = "救助"
     HelpRequest          = "支援要請"
-    LifeConFirmation     = "安否確認"
     Emergency            = "緊急支援"
     LifeLine             = "ライフライン"
     TransPortation       = "交通機関"
@@ -14,3 +13,4 @@ class Category(Enum):
     HealthInformation    = "医療・福祉・健康相談"
     LifeCareInformation  = "生活支援・相談"
     NoSetting            = "NoSetting"
+
